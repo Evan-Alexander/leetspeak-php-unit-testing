1b2411413f6e21caf97c73a40dcfab477b36c664
Jason Brown and Zach Swanson <zach@>
2017-02-13 11:52:00 -0800

    second test passes for replacing o to 0

--- src/LeetSpeak.php
+++ src/LeetSpeak.php
@@ -5,8 +5,13 @@
 
         function leetTranslate($input_string)
         {
-            return preg_replace("/e/i","3",$input_string);
+            $output_string = $input_string;
+            $output_string = preg_replace("/o/i","0", $output_string);
+            $output_string = preg_replace("/e/i","3",$output_string);
+            return $output_string;
         }
+
+
     }
 
 
--- tests/leetTest.php
+++ tests/leetTest.php
@@ -13,7 +13,19 @@
             $result = $test_object->leetTranslate($input);
 
             // Assert
-            $this->assertEquals("Don't you lov3 th3s3 'String' 3x3rcis3s? I do!", $result);
+            $this->assertEquals("D0n't y0u l0v3 th3s3 'String' 3x3rcis3s? I d0!", $result);
+        }
+
+        function test_substitute0()
+        {
+            // Arrange
+            $test_object = new LeetSpeak;
+            $input = "Don't you love these 'String' Exercises? I do!";
+            // Act
+            $result = $test_object->leetTranslate($input);
+
+            // Assert
+            $this->assertEquals("D0n't y0u l0v3 th3s3 'String' 3x3rcis3s? I d0!", $result);
         }
 
 
