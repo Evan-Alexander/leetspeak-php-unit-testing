c87ae01c476398b109cf0c2fe2dc575e04fa0dc6
Jason Brown and Zach Swanson <jason@>
2017-02-13 11:03:54 -0800

    initialize base files.  Created base for app.php

---
+++ app/app.php
@@ -0,0 +1,25 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../src/Leetspeak.php';
+    use Symfony\Component\Debug\Debug;
+    Debug::enable();
+
+    session_start();
+
+    $app = new Silex\Application();
+    $app['debug']=true;
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' =>__DIR__.'/../views'
+    ));
+
+    $app->get("/", function () use ($app) {
+
+    });
+
+    return $app;
+
+
+
+?>
